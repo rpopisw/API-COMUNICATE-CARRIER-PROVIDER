6362e1102788bcdedcfc9999fd4d05ab58d4b135
Popi Requis <rpopi@indracompany.com>
2022-03-16 17:52:22 -0500

    :rocket: feat(feature/request_generation_labels): Validation request

--- src/controllers/LabelShipping.js
+++ src/controllers/LabelShipping.js
@@ -1,7 +1,15 @@
 const HttpConstants = require('../supports/http.constants')
 const ServicesResponseDto = require('../dtos/ServicesResponseDto')
+const ServicesRequestDto = require('../dtos/ServicesRequestDto')
+const Validation = require('../validation')
+
 
 const createLabelShipping = async (event)=>{
+    const request = new ServicesRequestDto(event)
+    const error = await Validation.validateRequestCreateLabelShipping(request)
+    if(error){
+        return new ServicesResponseDto(HttpConstants.errorRequestResponse,error)
+    }
     return new ServicesResponseDto(HttpConstants.succefullResponse,'hello')
   }
 
---
+++ src/validation.js
@@ -0,0 +1,66 @@
+const Joi = require('joi')
+const CustomException = require('./supports/custom.exception')
+const ErrorContants = require('./supports/error.constants')
+
+const validateRequestCreateLabelShipping = async (request) => {
+    try {
+
+        const address_from = Joi.object({
+            name: Joi.string().required(),
+            street1: Joi.string().required(),
+            city: Joi.string().required(),
+            province: Joi.string().required(),
+            postal_code: Joi.string().required(),
+            country_code: Joi.string().required(),
+        })
+        const address_to = Joi.object({
+            name: Joi.string().required(),
+            street1: Joi.string().required(),
+            city: Joi.string().required(),
+            province: Joi.string().required(),
+            postal_code: Joi.string().required(),
+            country_code: Joi.string().required(),
+        })
+        const parcels = Joi.array().items(
+            Joi.object({
+                length: Joi.number().required(),
+                width: Joi.number().required(),
+                height: Joi.number().required(),
+                dimensions_unit: Joi.string().required(),
+                weight: Joi.number().required(),
+                weight_unit: Joi.string().required()
+            })
+        ).required()
+        const schema = Joi.object().keys({
+            payload: Joi.object().keys({
+                data: Joi.array().items(
+                    Joi.object({
+                        carrier: Joi.string().required(),
+                        shipment: Joi.object({
+                            address_from,
+                            address_to,
+                            parcels
+                        }).required()
+                    })
+                ).required()
+            }).required(),
+        }).required()
+
+
+
+        const response = await schema.validateAsync(request.body,{ abortEarly: false })
+        return response.error
+    } catch (error) {
+        console.log(error.details)
+        return new CustomException(
+            ErrorContants.requestInvalid.code,
+            ErrorContants.requestInvalid.message,
+            error.details,
+            ErrorContants.requestInvalid.httpStatus,
+        )
+    }
+}
+
+module.exports = {
+    validateRequestCreateLabelShipping
+}
